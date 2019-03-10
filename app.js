96186ba3448ecb397d99ce4e7d023044867cb64f
Alex Bukhman <alexb823@gmail.com>
2019-03-10 11:22:15 -0400

    done with the back end

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const { User, Thing, Favorite } = require('./db/index');
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/users', (req, res, next) => {
+  User.findAll({ include: [{ model: Favorite, include: Thing }] })
+    .then(usersAndFavorites => res.send(usersAndFavorites))
+    .catch(next);
+});
+
+app.get('/api/things', (req, res, next) => {
+  Thing.findAll({ include: [{ model: Favorite, include: User }] })
+    .then(thingsAndFavoritedBy => res.send(thingsAndFavoritedBy))
+    .catch(next);
+});
+
+//handle 404s
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+//error handling endware
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message || 'Internet server error');
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const server = require('./app');
+const { syncAndSeed } = require('./db/index');
 
-const port = process.env.DATABASE_URL || 3000;
+const port = process.env.PORT || 3000;
 
-
-server.listen(port, ()=> console.log(`Listening on port ${port}`))
+syncAndSeed().then(() =>
+  server.listen(port, () => console.log(`Listening on port ${port}`))
+);
